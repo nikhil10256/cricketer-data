fd57653e4dfa0c4b727cc95d492e8f764f7da5e1
nikhil10256 <nikhilsawant184@gmail.com>
2026-04-16 13:23:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let players = [
+  {
+    name: "Virat Kohli",
+    country: "India",
+    role: "Batsman",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Virat_Kohli.jpg"
+  },
+  {
+    name: "MS Dhoni",
+    country: "India",
+    role: "Wicketkeeper",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/7c/MS_Dhoni.jpg"
+  },
+  {
+    name: "Rohit Sharma",
+    country: "India",
+    role: "Batsman (Captain)",
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Rohit_Sharma.jpg"
+  }
+];
